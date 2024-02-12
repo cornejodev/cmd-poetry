5c382960e109fa3ec534583919291d0682879fc0
edmeister <koed.dev@gmail.com>
2024-02-11 17:02:10 -0700

    add: user input option

--- cmd_line_app_pyinstaller/main.py
+++ cmd_line_app_pyinstaller/main.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+
+
 # poetry run python cmd_line_app_pyinstaller/cmd_line_app.py
 def get_data():
-    response = requests.get('https://www.google.com')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    title = soup.find('title')
+    response = requests.get("https://www.google.com")
+    soup = BeautifulSoup(response.text, "html.parser")
+    title = soup.find("title")
     print(f"Hi, the title element of Google's webpage is: {title.text}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     get_data()
+    input("Press Enter to close...")
